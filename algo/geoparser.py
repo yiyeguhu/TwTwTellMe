11229a34219947b4325f71932b846e7936106b09
Tao Mao <yiyeguhu@gmail.com>
2015-04-05 19:16:04 -0700

    add geoparser.py

---
+++ algo/geoparser.py
@@ -0,0 +1,94 @@
+__author__ = 'tamao'
+
+USA = 'United States of America'
+OtherCountry = "Other Country"
+OtherState = "Other State"
+
+states = [
+        'Alaska',
+        'Alabama',
+        'Arkansas',
+        'Arizona',
+        'California',
+        'Colorado',
+        'Connecticut',
+        'District of Columbia',
+        'Delaware',
+        'Florida',
+        'Georgia',
+        'Hawaii',
+        'Iowa',
+        'Idaho',
+        'Illinois',
+        'Indiana',
+        'Kansas',
+        'Kentucky',
+        'Louisiana',
+        'Massachusetts',
+        'Maryland',
+        'Maine',
+        'Michigan',
+        'Minnesota',
+        'Missouri',
+        'Mississippi',
+        'Montana',
+        'North Carolina',
+        'North Dakota',
+        'Nebraska',
+        'New Hampshire',
+        'New Jersey',
+        'New Mexico',
+        'Nevada',
+        'New York',
+        'Ohio',
+        'Oklahoma',
+        'Oregon',
+        'Pennsylvania',
+        'Rhode Island',
+        'South Carolina',
+        'South Dakota',
+        'Tennessee',
+        'Texas',
+        'Utah',
+        'Virginia',
+        'Vermont',
+        'Washington',
+        'Wisconsin',
+        'West Virginia',
+        'Wyoming'
+]
+
+from geopy.geocoders import Nominatim
+geolocator = Nominatim()
+
+def parse_address(addr):
+    tokens = addr.split(', ')
+    l = len(tokens)
+
+    country = tokens[l-1]
+    if tokens[l-2].isdigit():
+        state = tokens[l-3]
+    else:
+        state = tokens[l-2]
+
+    if state not in states:
+        state = OtherState
+
+    if country != USA:
+        state = OtherState
+        country = OtherCountry
+
+    if state not in states:
+        state = OtherState
+
+    return state, country
+
+def parse_location(loc):
+    state, country = OtherState, OtherCountry
+
+    stdloc = geolocator.geocode(loc)
+    if stdloc is not None:
+        state, country = parse_address(stdloc.address)
+
+    return state, country
+
--- schema/python/tweet_pb2.py
+++ schema/python/tweet_pb2.py
@@ -18,7 +18,7 @@ _sym_db = _symbol_database.Default()
 DESCRIPTOR = _descriptor.FileDescriptor(
   name='tweet.proto',
   package='',
-  serialized_pb=_b('\n\x0btweet.proto\"(\n\x05Tweet\x12\x0c\n\x04text\x18\x01 \x02(\t\x12\x11\n\ttimestamp\x18\x02 \x02(\x05')
+  serialized_pb=_b('\n\x0btweet.proto\"H\n\x05Tweet\x12\x0c\n\x04text\x18\x01 \x02(\t\x12\x11\n\ttimestamp\x18\x02 \x02(\x05\x12\r\n\x05state\x18\x03 \x01(\t\x12\x0f\n\x07\x63ountry\x18\x04 \x01(\t')
 )
 _sym_db.RegisterFileDescriptor(DESCRIPTOR)
 
@@ -46,6 +46,20 @@ _TWEET = _descriptor.Descriptor(
       message_type=None, enum_type=None, containing_type=None,
       is_extension=False, extension_scope=None,
       options=None),
+    _descriptor.FieldDescriptor(
+      name='state', full_name='Tweet.state', index=2,
+      number=3, type=9, cpp_type=9, label=1,
+      has_default_value=False, default_value=_b("").decode('utf-8'),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None),
+    _descriptor.FieldDescriptor(
+      name='country', full_name='Tweet.country', index=3,
+      number=4, type=9, cpp_type=9, label=1,
+      has_default_value=False, default_value=_b("").decode('utf-8'),
+      message_type=None, enum_type=None, containing_type=None,
+      is_extension=False, extension_scope=None,
+      options=None),
   ],
   extensions=[
   ],
@@ -58,7 +72,7 @@ _TWEET = _descriptor.Descriptor(
   oneofs=[
   ],
   serialized_start=15,
-  serialized_end=55,
+  serialized_end=87,
 )
 
 DESCRIPTOR.message_types_by_name['Tweet'] = _TWEET
