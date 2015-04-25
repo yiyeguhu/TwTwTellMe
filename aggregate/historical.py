a4794e8a223dd8933e3339d293594d528bf4ae75
Tao Mao <yiyeguhu@gmail.com>
2015-04-25 13:02:27 -0700

    debug

--- aggregate/historical.py
+++ aggregate/historical.py
@@ -11,6 +11,7 @@ if __name__ == '__main__':
     start_time = get_start_of_hour(rough_start_time)
 
     while start_time < int(time.time()):
+        print 
         print start_time
         print unixtime_to_datetime(start_time)
         print aggregate(start_time, get_end_of_hour(start_time))
