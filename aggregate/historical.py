5cabc010b691df3480d49f0f03b21b82edaa4141
Tao Mao <yiyeguhu@gmail.com>
2015-04-25 13:02:04 -0700

    time info

--- aggregate/historical.py
+++ aggregate/historical.py
@@ -3,7 +3,7 @@ __author__ = 'tamao'
 import sys
 import time
 
-from aggregate.utils import aggregate, get_start_of_hour, get_end_of_hour, increase_by_hour
+from aggregate.utils import aggregate, get_start_of_hour, get_end_of_hour, increase_by_hour, unixtime_to_datetime
 
 if __name__ == '__main__':
     rough_start_time = int(sys.argv[1]) # 1429985000
@@ -11,5 +11,7 @@ if __name__ == '__main__':
     start_time = get_start_of_hour(rough_start_time)
 
     while start_time < int(time.time()):
+        print start_time
+        print unixtime_to_datetime(start_time)
         print aggregate(start_time, get_end_of_hour(start_time))
         start_time = increase_by_hour(start_time)
