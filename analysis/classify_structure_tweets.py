b5289eee25950029057dbdf097c93c479449dab9
Janak Mayer <janak.mayer@gmail.com>
2015-04-25 16:42:47 -0400

    Updating to talk to new clustered mongo

--- analysis/classify_structure_tweets.py
+++ analysis/classify_structure_tweets.py
@@ -7,34 +7,28 @@ from os import environ
 
 pwd = environ['MONGOPWD']
 
-client = pymongo.MongoClient(host="198.23.76.22", port=27017)
-client.newdb.authenticate('TwTw', pwd)
+client = pymongo.MongoClient(host="198.11.194.181", port=27017)
+# client.newdb.authenticate('TwTw', pwd)
 
 for tweet in client.newdb.tweets.find()[0:50]:
     dt = datetime.strptime(tweet['created_at'],'%a %b %d %H:%M:%S +0000 %Y')
+    ht = [hashtag['text'] for hashtag in tweet['entities']['hashtags']]
+    txt = tweet['text']
+    txtht = txt + ', '.join(ht)
+    # print tweet
     print ({
-        'text': tweet['text'],
+        'text': txt,
+        'hashtags':ht,
         'id': tweet['id'],
         'user_id': tweet['user']['id'],
         'user_name': tweet['user']['name'],
-        'candidate': return_candidates(tweet['text']),
-        'sentiment': return_sentiment(tweet['text']),
-        'themes': return_themes(tweet['text']),
+        'candidate': return_candidates(txtht),
+        'sentiment': return_sentiment(txtht),
+        'themes': return_themes(txtht),
         'timestamp': time.mktime(dt.timetuple())
     })
 
-
-# with open('../stream/search/tweet_0_2015-04-22.0.json','r') as f:
-#     data = json.load(f)
+# with open('jsonout.json', 'w') as json_file:
+#     json.dump([tweet for tweet in client.newdb.tweets.find()[0:50]], json_file)
+#     json_file.close()
 #
-# for tweet in data:
-#     dt = datetime.strptime(tweet['created_at'],'%a %b %d %H:%M:%S +0000 %Y')
-#     print ({
-#         'text': tweet['text'],
-#         'id': tweet['id'],
-#         'user_id': tweet['user']['id'],
-#         'user_name': tweet['user']['name'],
-#         'candidate': return_candidates(tweet['text']),
-#         'sentiment': return_sentiment(tweet['text']),
-#         'timestamp': time.mktime(dt.timetuple())
-#     })
