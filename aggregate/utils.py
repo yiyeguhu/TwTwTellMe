3bab64721531b5607576a5f1913289e3fe2d7949
Tao Mao <yiyeguhu@gmail.com>
2015-04-25 14:28:39 -0700

    add by-state sentiment

--- aggregate/utils.py
+++ aggregate/utils.py
@@ -5,6 +5,8 @@ from tweepy import Stream, OAuthHandler
 
 import argparse
 
+import us
+
 import sys
 
 from time import time, ctime, mktime
@@ -26,7 +28,7 @@ from datetime import datetime
 # from own packages
 from schema.python.tweet_pb2 import Tweet
 from protobufjson.protobuf_json import pb2json, json2pb
-from algo.geoparser import parse_location, OtherCountry, OtherState
+from algo.geoparser import parse_location, OtherCountry, OtherState, states
 from algo.dataminer import find_candidates, OtherCandidate
 from algo.tweet_check import return_candidates, return_sentiment
 from stream.utils import load_credentials, tweepy_auth
@@ -36,6 +38,8 @@ with open(filename) as f:
     candidates = json.load(f)
 
 print candidates
+
+state_map = us.states.mapping('name', 'abbr')
 
 client = MongoClient('127.0.0.1', 27018) # new port 27018
 # collection = client['test']['testData']
@@ -67,7 +71,6 @@ def get_sentiment_stats_for_candidate(cand, starttime, endtime):
         pipeline = [filter_for_candidate, stats]
         ret = collection.aggregate(pipeline)
 
-
         if ret['ok'] == 1.0:
             buckets = ret['result']
             for bucket in buckets:
@@ -80,17 +83,51 @@ def get_sentiment_stats_for_candidate(cand, starttime, endtime):
 def _init_sentiment_buckets():
     return {'1':0, '2':0, '3':0, '4':0, '5':0, '6':0}
 
+def get_sentiment_stats_by_state_for_candidate(cand, starttime, endtime):
+
+    try:
+        state_buckets = _init_sentiment_buckets_by_state()
+
+        filter_for_candidate  = {'$match': { 'candidate' : cand, 'timestamp' : { '$gt': starttime, '$lt': endtime } } }
+        stats = {"$group": {"_id": {"sentiment_int" : "$sentiment_int", "state" : "$state"}, "count": {"$sum": 1} } }
+
+        pipeline = [filter_for_candidate, stats]
+        ret = collection.aggregate(pipeline)
+
+        if ret['ok'] == 1.0:
+            buckets = ret['result']
+            for bucket in buckets:
+                if 'sentiment_int' in bucket['_id'] and 'state' in bucket['_id']:
+                    state = bucket['_id']['state']
+                    sentiment_int = bucket['_id']['sentiment_int']
+                    count = bucket['count']
+
+                    state_buckets[state_map[state]][str(sentiment_int)] = count
+
+        return state_buckets
+    except:
+        return _init_sentiment_buckets_by_state()
+
+def _init_sentiment_buckets_by_state():
+    state_buckets = {}
+    for state in states:
+        state_buckets[state_map[state]] = _init_sentiment_buckets()
+
+    return state_buckets
+
 def get_aggregate_for_candidate(cand, starttime, endtime):
     cand_info = {"tweets":{}, "sentiment_scores": {}}
 
     # tweets
 
     # sentiment:
-    # (1)all states
+     # (1)all states
     all_states = get_sentiment_stats_for_candidate(cand, starttime, endtime)
     cand_info['sentiment_scores']['All States'] = all_states
 
-    # by state
+    # (2) by state
+    sentiment_by_state = get_sentiment_stats_by_state_for_candidate(cand, starttime, endtime)
+    cand_info['sentiment_scores']['States'] = sentiment_by_state
 
     return cand_info
 
@@ -104,9 +141,6 @@ def aggregate(starttime, endtime):
         return result
     except:
         return {'candidate_data':{}}
-
-def get_sentiment_stats_by_state_for_candidate():
-    pass
 
 if __name__ == '__main__':
     rough_start_time = int(sys.argv[1])
