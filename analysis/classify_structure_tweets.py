b0f2060eebb8eee27ac31cf80b7982e606e41106
Janak Mayer <janak.mayer@gmail.com>
2015-04-26 15:27:57 -0400

    updates to classify_structure

--- analysis/classify_structure_tweets.py
+++ analysis/classify_structure_tweets.py
@@ -1,53 +1,12 @@
 import pymongo
 from algo.tweet_check import return_candidates, return_sentiment, return_themes
-from datetime import datetime, tzinfo
+from dateutil import parser
 import time
+import calendar
+from datetime import datetime
 import json
+from math import floor, ceil
 from os import environ
-
-pwd = environ['MONGOPWD']
-
-client = pymongo.MongoClient(host="198.11.194.181", port=27017)
-# client.newdb.authenticate('TwTw', pwd)
-
-for tweet in client.newdb.tweets.find()[0:50]:
-    dt = datetime.strptime(tweet['created_at'],'%a %b %d %H:%M:%S +0000 %Y')
-    print tweet['created_at'], dt
-    ht = [hashtag['text'] for hashtag in tweet['entities']['hashtags']]
-    txt = tweet['text']
-    txtht = txt + ', '.join(ht)
-    processed = {
-        'text': txt,
-        'hashtags':ht,
-        'id': tweet['id'],
-        'user_id': tweet['user']['id'],
-        'user_name': tweet['user']['name'],
-        'screen_name': tweet['user']['screen_name'],
-        'raw_location': tweet['user'].get('location', 'not specified'),
-        'candidate': return_candidates(txtht),
-        'sentiment': return_sentiment(txtht),
-        'themes': return_themes(txtht),
-        'timestamp': time.mktime(dt.timetuple())
-    }
-
-
-    # print processed
-    # try:
-    #     client.newdb.processed_tweets.insert(processed, continue_on_error=True)
-    #     print 'inserted processed tweet'
-    # except pymongo.errors.DuplicateKeyError:
-    #     print 'duplicate key - skipping'
-    #
-    # tweet['processed'] = True
-    # client.newdb.tweets.update({'_id': tweet['_id']}, {"$set": tweet}, upsert=False)
-    # print 'updated processed flag'
-
-
-# with open('jsonout.json', 'w') as json_file:
-#     json.dump([tweet for tweet in client.newdb.tweets.find()[0:50]], json_file)
-#     json_file.close()
-#
-
 
 # Convert (-1,1) sentiment: (-1,-.5] -> 1, (.5,0] -> 2, 0 -> 3, [0,.5) -> 4, [.5,1) -> 5
 def convert_sent(f):
@@ -60,4 +19,40 @@ def convert_sent(f):
         f = 1
     elif f > 5:
         f = 5
-    print f
+    return int(f)
+
+pwd = environ['MONGOPWD']
+
+client = pymongo.MongoClient(host="198.11.194.181", port=27017)
+
+counter = 0
+for tweet in client.newdb.tweets.find()[0:50]:
+    dt = parser.parse(tweet['created_at'])
+    ht = [hashtag['text'] for hashtag in tweet['entities']['hashtags']]
+    txt = tweet['text']
+    txtht = txt + ', '.join(ht)
+    sent = return_sentiment(txtht)
+    processed = {
+        'text': txt,
+        'hashtags':ht,
+        'id': tweet['id'],
+        'user_id': tweet['user']['id'],
+        'user_name': tweet['user']['name'],
+        'screen_name': tweet['user']['screen_name'],
+        'raw_location': tweet['user'].get('location', 'not specified'),
+        'candidate': return_candidates(txtht),
+        'sentiment': sent,
+        'sentiment_int': convert_sent(sent),
+        'themes': return_themes(txtht),
+        'timestamp': calendar.timegm(dt.timetuple())
+    }
+    try:
+        client.newdb.processed_tweets.insert(processed, continue_on_error=True)
+        counter += 1
+        print counter
+    except pymongo.errors.DuplicateKeyError:
+        print 'duplicate key - skipping'
+
+    tweet['processed'] = True
+    client.newdb.tweets.update({'_id': tweet['_id']}, {"$set": tweet}, upsert=False)
+
