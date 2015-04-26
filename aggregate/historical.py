2801a9c141f1f0b3bf789c695bf48d5518f72bf2
Tao Mao <yiyeguhu@gmail.com>
2015-04-26 10:50:33 -0700

    add realtime.py

--- aggregate/historical.py
+++ aggregate/historical.py
@@ -10,7 +10,8 @@ from aggregate.utils import aggregate, get_start_of_hour, get_end_of_hour, incre
 #r_server = redis.Redis(host='198.23.67.172', password='dupont')
 
 if __name__ == '__main__':
-    # '198.23.67.172' 'password' 1429985000->1430002054
+    # [redis-hostname] [redis-pw] [starttime]
+    # '198.23.67.172' 'password' 1429985000->1430002054(hashtags)
     h = sys.argv[1]
     p = sys.argv[2]
 
