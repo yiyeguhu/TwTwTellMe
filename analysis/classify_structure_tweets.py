dff566d00b273ff291f32e16644be01813b9d828
mattmag89 <mhayes@ischool.berkeley.edu>
2015-04-26 15:18:16 -0400

    built return_candidates wrapper to check urls if no candidate found

--- analysis/classify_structure_tweets.py
+++ analysis/classify_structure_tweets.py
@@ -47,3 +47,16 @@ for tweet in client.newdb.tweets.find()[0:50]:
 #     json.dump([tweet for tweet in client.newdb.tweets.find()[0:50]], json_file)
 #     json_file.close()
 #
+
+# Convert (-1,1) sentiment: (-1,-.5] -> 1, (.5,0] -> 2, 0 -> 3, [0,.5) -> 4, [.5,1) -> 5
+def convert_sent(f):
+    f = 2*f + 3
+    if f < 3:
+        f = floor(f)
+    elif f > 3:
+        f = ceil(f)
+    if f < 1:
+        f = 1
+    elif f > 5:
+        f = 5
+    print f
