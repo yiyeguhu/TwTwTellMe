80f097c39a437ccccbee1637bee084ceedf934a3
Tao Mao <yiyeguhu@gmail.com>
2015-04-29 21:31:22 -0700

    fix realtime

--- aggregate/utils.py
+++ aggregate/utils.py
@@ -75,14 +75,12 @@ def aggregate_tweets_for_candidate(cand, starttime, endtime):
         # f = {'candidate' : cand, 'timestamp' : { '$gt': starttime, '$lt': endtime }}
         # pr = {'user_name':1, 'state':1, 'text':1, 'themes':1, 'hashtags':1, 'sentiment_int':1, '_id':0}
 
-        ret = collection.find({'candidate' : cand, 'timestamp' : { '$gt': starttime, '$lt': endtime }}, {'user_name':1, 'state':1, 'text':1, 'themes':1, 'hashtags':1, 'sentiment_int':1, '_id':0}, limit=10)
+        ret = collection.find({'candidate' : cand, 'timestamp' : { '$gt': starttime, '$lt': endtime }}, {'user_name':1, 'text':1, 'themes':1, 'hashtags':1, 'sentiment_int':1, '_id':0}, limit=10)
 
         for doc in ret:
             item = {}
             if 'user_name' in doc:
                 item['user_name'] = doc['user_name']
-            if 'state' in doc:
-                item['user_state'] = doc['state']
             if 'text' in doc:
                 item['tweet_text'] = doc['text']
             if 'themes' in doc:
