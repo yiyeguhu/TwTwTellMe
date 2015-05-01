9849fdd9536627734887026d7759c4da2f71893e
Tao Mao <yiyeguhu@gmail.com>
2015-04-30 18:29:59 -0700

    fix

--- analysis/classify_structure_tweets.py
+++ analysis/classify_structure_tweets.py
@@ -40,7 +40,9 @@ if __name__ == '__main__':
     # for tweet in src_collection.find({'timestamp': {'$gte': 1430247600, '$lt': 1430301600}}):
     # for tweet in src_collection.find({'timestamp': {'$gte': 1430410447}}):
     # for tweet in src_collection.find({'timestamp': {'$gte': 1430301600, '$lt': 1430312400}}):
-    for tweet in src_collection.find({'timestamp': {'$gte': 1430413200, '$lt': 1430409600}}):
+    # for tweet in src_collection.find({'timestamp': {'$gte': 1430413200, '$lt': 1430409600}}):
+
+    for tweet in src_collection.find({'timestamp': {'$gte': 1430440140}}):
         try:
             dt = parser.parse(tweet['created_at'])
 
