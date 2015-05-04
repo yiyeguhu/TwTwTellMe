71fcc13012590f894d08d41137772c99cd40f5d8
Tao Mao <yiyeguhu@gmail.com>
2015-05-04 02:12:41 -0700

    fix historical

--- aggregate/historical.py
+++ aggregate/historical.py
@@ -13,21 +13,27 @@ from aggregate.utils import aggregate, get_start_of_hour, get_end_of_hour, incre
 
 if __name__ == '__main__':
     # [redis-hostname] [redis-pw] [starttime]
-    # '198.23.67.172' 'password' 1429985000->1430002054(hashtags)->142999784(ETL from Mongo Cluster)
+    # '198.23.67.172' 'password'
+    # starttime: 1429985000->1430002054(hashtags)->142999784(ETL from Mongo Cluster)
+    # endtime:
     h = sys.argv[1]
     p = sys.argv[2]
+    r = redis.Redis(host=h, password=p)
 
     rough_start_time = int(sys.argv[3])
     print rough_start_time
     start_time = get_start_of_hour(rough_start_time)
 
-    r = redis.Redis(host=h, password=p)
+    end_time = int(sys.argv[4])
 
-    while start_time < int(time.time()):
-        print start_time
+    # while start_time < int(time.time()):
+    while start_time < end_time:
         # unixtime_to_datetime(start_time)
         hour_data = aggregate(start_time, get_end_of_hour(start_time))
 
         r.set(str(start_time), json.dumps(hour_data))
+
+        print start_time
+        print hour_data
 
         start_time = increase_by_hour(start_time)
