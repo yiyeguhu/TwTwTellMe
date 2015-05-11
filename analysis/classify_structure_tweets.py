1a9d3915bd498bc9980b515e01fc0fbb06c27965
Tao Mao <yiyeguhu@gmail.com>
2015-05-11 09:22:24 -0700

    query from (1431036000, 1431050400)

--- analysis/classify_structure_tweets.py
+++ analysis/classify_structure_tweets.py
@@ -60,7 +60,8 @@ if __name__ == '__main__':
     # for tweet in src_collection.find({'timestamp': {'$gt': 1431014400, '$lt': 1431050400}}):
     # for tweet in src_collection.find({'timestamp': {'$gt': 1431018000, '$lt': 1431050400}}):
     # for tweet in src_collection.find({'timestamp': {'$gt': 1431025200, '$lt': 1431050400}}):
-    for tweet in src_collection.find({'timestamp': {'$gt': 1431028800, '$lt': 1431050400}}):
+    # for tweet in src_collection.find({'timestamp': {'$gt': 1431028800, '$lt': 1431050400}}):
+    for tweet in src_collection.find({'timestamp': {'$gt': 1431036000, '$lt': 1431050400}}):
         try:
             dt = parser.parse(tweet['created_at'])
 
